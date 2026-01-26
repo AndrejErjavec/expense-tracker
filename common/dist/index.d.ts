5a937731abcecdaad7c3b7abcacee9a46bd9e891
Andrej Erjavec <erandrej@gmail.com>
2026-01-27 00:12:48 +0100

    monorepo setup 2

--- common/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { supabase, TransactionInsert, TransactionUpdate } from "./supabase";
-export type { Database, Tables } from "./supabase/database.types";
-//# sourceMappingURL=index.d.ts.map
--- common/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabase = void 0;
-var supabase_1 = require("./supabase");
-Object.defineProperty(exports, "supabase", { enumerable: true, get: function () { return supabase_1.supabase; } });
-//# sourceMappingURL=index.js.map
