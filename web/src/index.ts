14b58463ac79d8d6ebf8e4426ee2f08a75d8d85c
Andrej Erjavec <erandrej@gmail.com>
2026-01-27 00:17:36 +0100

    removed env from common

--- web/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import app from "./app";
-
-const port = Number(process.env.PORT ?? 3000);
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on port ${port}`);
-});
--- web/src/utils/date.ts
+++
@@ -1,6 +0,0 @@
-import { parse, format } from "date-fns";
-
-export const formatToIso = (date: string) => {
-  const d = parse(date, "d.M.yyyy", new Date());
-  return format(d, "yyyy-MM-dd");
-};
